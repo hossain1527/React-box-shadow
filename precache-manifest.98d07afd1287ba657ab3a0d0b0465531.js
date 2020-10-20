41d47d932b4514e848ae52327d687b929d6767f8
hossain1527 <hossain.sci2015@gmail.com>
2020-10-20 15:34:08 +0100

    Updates

---
+++ precache-manifest.98d07afd1287ba657ab3a0d0b0465531.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6667653d45940547ac8cee7cdc1a798a",
+    "url": "/React-box-shadow/index.html"
+  },
+  {
+    "revision": "b2dfdd5e6591b4c434db",
+    "url": "/React-box-shadow/static/css/main.c65d1318.chunk.css"
+  },
+  {
+    "revision": "2c2f64b933d144a5ed32",
+    "url": "/React-box-shadow/static/js/2.be91d332.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-box-shadow/static/js/2.be91d332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2dfdd5e6591b4c434db",
+    "url": "/React-box-shadow/static/js/main.a13a477f.chunk.js"
+  },
+  {
+    "revision": "3a1638108039e1462488",
+    "url": "/React-box-shadow/static/js/runtime-main.96606626.js"
+  }
+]);
